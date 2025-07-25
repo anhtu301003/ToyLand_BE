2af85712ec0a8c3bc6d09b8c300354835c2dc46c
AnhTu <tuanhdao2@gmail.com>
2025-07-25 17:14:42 +0700

    update eureka server

--- inventory-service/src/main/java/com/toyland/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/toyland/inventory_service/InventoryServiceApplication.java
@@ -2,12 +2,14 @@ package com.toyland.inventory_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
 
 }
--- order-service/src/main/java/com/toyland/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/toyland/order_service/OrderServiceApplication.java
@@ -2,16 +2,18 @@ package com.toyland.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
 
 }
